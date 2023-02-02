849cc0e931dfd212cc2cbccc778116f59bfa3ea1
Dev Kumar <devskumar99@gmail.com>
2023-02-02 12:09:45 -0500

    data functions for hw3
    Signed-off-by: Dev Kumar <devskumar99@gmail.com>

--- HW3/src/main.py
+++ HW3/src/main.py
@@ -10,9 +10,13 @@ USAGE:   script.lua  [OPTIONS] [-g ACTION]
 OPTIONS:
   -d  --dump  on crash, dump stack = false
   -f  --file    name of file       = data/data.csv
+  -F  --Far     distance to "faraway"  = .95	
   -g  --go    start-up action      = data
   -h  --help  show help            = false
+  -m  --min     stop clusters at N^min = .5
+  -p  --p       distance coefficient   = 2
   -s  --seed  random number seed   = 937162211
+  -S  --Sample  sampling data size     = 512
 ACTIONS:
   -g  the	show settings
   -g  sym	check syms
@@ -20,6 +24,12 @@ ACTIONS:
   -g  csv	read from csv
   -g  data	read DATA csv
   -g  stats	stats from DATA
+  -g  clone	duplicate structure
+  -g  around	sorting nearest neighbors
+  -g  half	1-level bi-clustering
+  -g  cluster	N-level bi-clustering
+  -g  optimize	semi-supervised optimization
+
 """
 
 the = cli(sys.argv[1:])
