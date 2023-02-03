bdf397c75d436bd931c22a66fe0cbdf518cb297a
palash27 <36497688+palash27@users.noreply.github.com>
2023-02-02 21:42:14 -0500

    Update misc.py

--- HW3/src/misc.py
+++ HW3/src/misc.py
@@ -11,9 +11,13 @@ USAGE:   script.lua  [OPTIONS] [-g ACTION]
 OPTIONS:
   -d  --dump  on crash, dump stack = false
   -f  --file    name of file       = data/data.csv
-  -g  --go    start-up action      = data
-  -h  --help  show help            = false
-  -s  --seed  random number seed   = 937162211
+  -F  --Far     distance to "faraway"  = .95
+  -g  --go      start-up action        = data
+  -h  --help    show help              = false
+  -m  --min     stop clusters at N^min = .5
+  -p  --p       distance coefficient   = 2
+  -s  --seed    random number seed     = 937162211
+  -S  --Sample  sampling data size     = 512
 ACTIONS:
   -g  the	show settings
   -g  sym	check syms
