c9f53a8224082dbc87a105c4ad465ca9aef294f7
palash27 <rathodpalash3@gmail.com>
2023-02-20 16:03:46 -0500

    Add data functions

---
+++ HW5/src/Data.py
@@ -0,0 +1,16 @@
+from misc import *
+from Row import *
+def new_data():
+    return {'rows':{}, 'cols': None}
+
+def read_data(the, sfile):
+    data = new_data()
+    def fun(t):
+        row(the, data,t)
+    csv(sfile, fun)
+    return data
+
+def data_clone(data, ts={}):
+    data1 = row(new_data(), data['cols']['names'])
+    for _, t in ts.items():
+        row(data1, t)
---
+++ HW5/src/__init__.py
@@ -0,0 +1,6 @@
+from src.misc import *
+from src.Num import *
+from src.main import *
+from src.Cols import *
+from src.Sym import *
+from src.Row import *
