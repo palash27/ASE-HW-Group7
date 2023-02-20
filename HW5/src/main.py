6140a966ae76a94b3ed25e72afed470caf547203
palash27 <rathodpalash3@gmail.com>
2023-02-20 16:08:00 -0500

    add main

---
+++ HW5/src/main.py
@@ -0,0 +1,39 @@
+from misc import cli
+import sys
+from test_engine import test
+
+
+help = """   
+script.lua : an example script with help text and a test suite
+(c)2022, Tim Menzies <timm@ieee.org>, BSD-2 
+USAGE:   script.lua  [OPTIONS] [-g ACTION]
+OPTIONS:
+  -b  --bins    initial number of bins       = 16
+  -c  --cliffs  cliff's delta threshold      = .147
+  -f  --file    data file                    = data/data.csv
+  -F  --Far     distance to distant          = .95
+  -g  --go      start-up action              = nothing
+  -h  --help    show help                    = false
+  -H  --Halves  search space for clustering  = 512
+  -m  --min     size of smallest cluster     = .5
+  -M  --Max     numbers                      = 512
+  -p  --p       dist coefficient             = 2
+  -r  --rest    how many of rest to sample   = 4
+  -R  --Reuse   child splits reuse a parent pole = true
+  -s  --seed    random number seed           = 937162211
+ACTIONS:
+  -g  the	show settings
+  -g  sym	check syms
+  -g  num	check nums
+  -g  csv	read from csv
+  -g  data	read DATA csv
+  -g  stats	stats from DATA
+"""
+
+the = cli(sys.argv[1:])
+
+test(the)
+if the['help']:
+    exit (print("\n" + str(help) + "\n"))
+
+
