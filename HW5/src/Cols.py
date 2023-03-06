a9c930c5ec14bef0f5971bb561d9c378d82d36db
palash27 <rathodpalash3@gmail.com>
2023-03-06 00:03:45 -0500

    update hw5 cols.py

--- HW5/src/Cols.py
+++ HW5/src/Cols.py
@@ -9,7 +9,11 @@ def col_f(n, s):
   -- e.g. makes goals if the name ends with
   -- the maximize (`+`) or minimize (`-`) or klass (`!`) symbol.
   """
-  col = num(n, s) if s.startswith("^[A-Z]") else sym(n, s)
+  if re.match("^[A-Z]+", s) == None:
+    col = sym(n,s)
+  else:
+    col = num(n,s)
+  # col = num(n, s) if s.startswith("^[A-Z]") else sym(n, s)
   col['isIgnored'] = True if s.endswith("X") else False
   col['isKlass'] = True if s.endswith("!") else False
   col['isGoal'] = True if s.endswith("!") or s.endswith("+") or s.endswith("-") else False
@@ -34,7 +38,11 @@ def Cols(ss):
     if not col['isIgnored']:
       if col['isKlass']:
         cols['klass']=col
-      push(col['isGoal'] and cols['y'] or cols['x'], col)
+      if col['isGoal']:
+        push(cols['y'], col)
+      else:
+        push(cols['x'], col)
+      # push(col['isGoal'] and cols['y'] or cols['x'], col)
   return cols
 
 
